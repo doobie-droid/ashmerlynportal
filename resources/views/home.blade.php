6f26cc2b4bd4ec7a85f7ce6070b86882ff7803d2
Douglas Leslie <lesliedouglas23@gmail.com>
2022-11-20 17:29:43 +0100

    feat: create user interface for admin side of the dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div>Yup this is the homepage</div>
+    <h1>{{'The boy has a username of '.$loggedinuser->username.' and a first name of'.$loggedinuser->firstname}} </h1>
     @endsection
 
 </x-portal-layout>
