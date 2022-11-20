29562b5b390f3ce3616289855d6dcf8a5a178051
Douglas Leslie <lesliedouglas23@gmail.com>
2022-11-20 09:44:56 +0100

    the layout for the user interface has been created

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
 
 Auth::routes();
+Route::group(['middleware' => 'auth'], function(){
+//    Route::get('/home', function (){
+//        return view('home');
+//    })->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+
+
+
