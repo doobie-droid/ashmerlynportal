527576de2ee5fd8d9a756a56f0a7308714838782
doobie-droid <lesliedouglas23@gmail.com>
2024-05-02 22:45:08 +0100

    style: updated UserFactory to match pep8

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -18,18 +19,18 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'username'=>fake()->userName(),
+            'username' => fake()->userName(),
             'firstname' => fake()->firstName(),
             'middlename' => fake()->firstName(),
             'surname' => fake()->lastName(),
-            'year_id'=>fake()->numberBetween(1,6),
-            'arm_id'=>fake()->numberBetween(1,6),
+            'year_id' => fake()->numberBetween(1, 6),
+            'arm_id' => fake()->numberBetween(1, 6),
             'email' => fake()->unique()->safeEmail(),
-            'phone_number'=>fake()->phoneNumber(),
-            'date_of_birth'=>fake()->dateTimeThisCentury(),
-            'gender'=>'male',
+            'phone_number' => fake()->phoneNumber(),
+            'date_of_birth' => fake()->dateTimeThisCentury(),
+            'gender' => 'male',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' =>  Hash::make('password'), // password
             'remember_token' => Str::random(10),
         ];
     }
